a8b97a81810c479c2f795164608fe824d69b5823
Allan Nava <allannava95@gmail.com>
2023-01-16 12:20:07 +0100

    added "cd ffmpeg"

--- ffmpeg-compile.sh
+++ ffmpeg-compile.sh
@@ -22,6 +22,7 @@ apt-get install -y build-essential yasm cmake libtool libc6 libc6-dev unzip wget
 #
 pwd 
 ls -l
+cd ffmpeg 
 #
 ./configure --enable-nonfree --enable-cuda-nvcc --enable-libnpp --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64 --disable-static --enable-shared
 #
