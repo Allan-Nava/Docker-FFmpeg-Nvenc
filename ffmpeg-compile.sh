3e7d7d78cf632d825cb55e8df04227d7c64cdea6
Allan Nava <allannava95@gmail.com>
2023-01-16 11:24:51 +0100

    removed sudo

--- ffmpeg-compile.sh
+++ ffmpeg-compile.sh
@@ -8,18 +8,18 @@
 # Abort on error
 set -e
 #
-sudo apt-get install git 
+apt-get install git 
 #
 git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
 
-cd nv-codec-headers && sudo make install && cd –
+cd nv-codec-headers &&  make install && cd –
 
 git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg/
 
-sudo apt-get install build-essential yasm cmake libtool libc6 libc6-dev unzip wget libnuma1 libnuma-dev
+apt-get install build-essential yasm cmake libtool libc6 libc6-dev unzip wget libnuma1 libnuma-dev
 
 ./configure --enable-nonfree --enable-cuda-nvcc --enable-libnpp --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64 --disable-static --enable-shared
 
 make -j 8
 
-sudo make install
+make install
