55584d5fcab59f1a818f9828e89ce060fb6197de
Allan Nava <allannava95@gmail.com>
2023-01-16 11:17:24 +0100

    update ffmpeg-compile.sh

--- ffmpeg-compile.sh
+++ ffmpeg-compile.sh
@@ -1,7 +1,23 @@
+#!/bin/bash
+# Automatically compile and install FFMPEG with NVIDIA hardware acceleration on Debian 11
+# Includes cuvid, cuda, nvenc, nvdec, and non-free libnpp
+# Based on:
+#  https://www.tal.org/tutorials/ffmpeg_nvidia_encode
+#  https://developer.nvidia.com/blog/nvidia-ffmpeg-transcoding-guide/
+#
+# Abort on error
+set -e
+#
 git clone https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
+
 cd nv-codec-headers && sudo make install && cd –
+
 git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg/
+
 sudo apt-get install build-essential yasm cmake libtool libc6 libc6-dev unzip wget libnuma1 libnuma-dev
+
 ./configure --enable-nonfree --enable-cuda-nvcc --enable-libnpp --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64 --disable-static --enable-shared
+
 make -j 8
+
 sudo make install
