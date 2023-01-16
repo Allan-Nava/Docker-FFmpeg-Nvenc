8ff8ec4a2f908af8faafa29ff6c51c738ccff9d7
Allan Nava <allannava95@gmail.com>
2023-01-16 11:45:15 +0100

    revert ./compile

--- ffmpeg-compile.sh
+++ ffmpeg-compile.sh
@@ -20,7 +20,7 @@ git clone https://git.ffmpeg.org/ffmpeg.git ffmpeg/
 
 apt-get install -y  build-essential yasm cmake libtool libc6 libc6-dev unzip wget libnuma1 libnuma-dev
 
-configure --enable-nonfree --enable-cuda-nvcc --enable-libnpp --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64 --disable-static --enable-shared
+./configure --enable-nonfree --enable-cuda-nvcc --enable-libnpp --extra-cflags=-I/usr/local/cuda/include --extra-ldflags=-L/usr/local/cuda/lib64 --disable-static --enable-shared
 
 make -j 8
 
